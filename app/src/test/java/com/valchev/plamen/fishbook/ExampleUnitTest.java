6e22ce4304aa5b4c703ba2498457d97fb5347feb
pvalchev <plvalchev@abv.bg>
2017-02-26 13:14:00 +0200

    Home screen

--- app/src/test/java/com/valchev/plamen/fishbook/ExampleUnitTest.java
+++ app/src/test/java/com/valchev/plamen/fishbook/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.valchev.plamen.fishbook;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
